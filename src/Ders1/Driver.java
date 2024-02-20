3d2c2d1d73f5e9d5cf8904aa13176ffb5638377c
hkinay <kinayhuseyin@gmail.com>
2024-02-20 18:08:58 +0300

    İlk hafta Java Basics Anlatıldı

--- src/Ders1/Driver.java
+++ src/Ders1/Driver.java
@@ -1,5 +1,6 @@
 package Ders1;
 
+import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,9 +8,13 @@ public class Driver {
 
     public static void main(String[] args) {
 
+        dateSinifiile_printfOrnekleri();
+
+       // iciceDongu();
+       // carpimTablosu();
         //kosulOrnekleri();
         //donguOrnekleri();
-        sayiTahminOyunu();
+        //sayiTahminOyunu();
         /*
         Scanner nesne = new Scanner(System.in);
         System.out.println("Faktöriyel için bir sayı ver: ");
@@ -19,25 +24,29 @@ public class Driver {
 */
 
 
+
     }
 
     private static void sayiTahminOyunu() {
         int hak = 3;
-
         Random random = new Random();
         int uretilen = random.nextInt(100);
         Scanner sayi = new Scanner(System.in);
-
-
         while (hak > 0){
-            System.out.println("Sayı tahmin ediniz: ");
+            System.out.println("0-100 arası sayı tahmin ediniz: ");
             int ksayi = sayi.nextInt();
+            hak -= 1;
+           // System.out.println("bilgisayar sayisi " + uretilen);
             if (uretilen == ksayi){
                 System.out.println("Tebrikler");
                 break;
-                
             }
-            hak -= 1;
+            else{
+                System.out.printf("Bilemediniz, Kalan Hakkınız %d%n",hak);
+            }
+            if(hak==0){
+                System.out.println("Hakkınız bitti");
+            }
 
         }
     }
@@ -135,6 +144,47 @@ while (sayi <= faktor ){
             default:
                 System.out.println("doğru gün sayısı girilmedi");
         }
+
+    }
+
+    public static void iciceDongu (){
+        int boyut = 5;
+        for (int i = 0; i < boyut ; i++) {
+            System.out.print("* ");
+            for (int j = 0; j < boyut; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+    public static void carpimTablosu (){
+        int satir = 10;
+        int sutun = 10;
+
+        for (int i = 0; i <= satir; i++) {
+            System.out.printf("%2d ",i);
+            for (int j = 0; j <= sutun; j++) {
+
+                System.out.printf("%2d ", i*j);
+
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void dateSinifiile_printfOrnekleri(){
+        Date bugun = new Date();
+        System.out.printf("Gün tarih ve saati %s%n",bugun);
+        System.out.printf("türkçe olarak tarih %tc%n",bugun);
+        System.out.printf("yıl gün ay olarak %tF%n",bugun);
+        System.out.printf("ay gün yıl olarak %tD%n",bugun);
+        System.out.printf("PM olarak %tR%n",bugun);
+        System.out.printf("AM olarak %tr%n",bugun);
+
 
     }
 
