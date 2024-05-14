f28a242be8c0672d64aa15c7f601d9a1b7ae25b3
hkinay <kinayhuseyin@gmail.com>
2024-05-15 00:08:07 +0300

    10. hafta Veritabanı Bağlantı (mysql ve sqlserver)

---
+++ src/Ders10/SqlServerBaglan.java
@@ -0,0 +1,55 @@
+package Ders10;
+
+import java.sql.*;
+
+public class SqlServerBaglan {
+    public static void main(String[] args) {
+
+        Connection conn = null;
+
+
+
+
+
+        //String connectionURL="jdbc:jtds:sqlserver://{#host::.::[}/{database::::[}?[;<;,instance={localdb_pipe},user={user:param:::[},password={password:param:::[},{:identifier}={:param:::[}>]";
+
+
+        try
+
+        {
+            String connectionURL = "jdbc:jtds:sqlserver://./";
+
+            conn = DriverManager.getConnection(connectionURL);
+            if(conn!=null){
+                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+
+            }
+            Statement stmt = conn.createStatement();
+            String SQL = "Select urun_id,satici_id From urunler";
+            ResultSet resultSet = stmt.executeQuery(SQL);
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("Ürün ID") + " " + resultSet.getString("Satıcı ID"));
+            }
+
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+}
